8829d6f3f6d624c12431d3260400c26e1b5ef3ff
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-10 13:51:27 +0300

    method creation

--- person.cs
+++ person.cs
@@ -2,5 +2,16 @@ using System;
 namespace PersonExample
 {
     // Define the Person class
-    
+    class Person
+    {
+        // Properties
+        public string Name {get; set;}
+        public int Age {get; set;}
+
+        // Method to display person details
+        public void DisplayDetails()
+        {
+            console
+        }
+    }
 }
